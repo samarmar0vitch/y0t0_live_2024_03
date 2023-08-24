510303fe63b6565a68e938061f3415440cc3353d
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-08-24 14:58:22 +0100

    Update startup.sh

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -7,6 +7,7 @@ pwd
 git config pull.ff 
 git clone https://github.com/l00ke3/lia_pro.git /root/lia_pro
 chmod +x /root/lia_pro/script_ffmpeg.sh
+sed -i "s/babajackson84/$live_user/g" /root/lia_pro/rigle.py
 
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
