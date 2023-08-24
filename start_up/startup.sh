d62a88f7f25218e040c583b6b1ec5957a13a4ded
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-08-24 13:22:50 +0100

    Update startup.sh

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -2,5 +2,6 @@
 
 echo "------------------- START - UP ----------------------------"
 pwd
+git clone https://github.com/l00ke3/lia_pro.git /root/
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
