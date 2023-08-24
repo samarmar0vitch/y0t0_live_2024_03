875bc06e685e1395b9a4d305de7b88cb88a7c5b9
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-08-24 01:13:40 +0100

    Update sync.sh

--- start_up/sync.sh
+++ start_up/sync.sh
@@ -3,16 +3,18 @@
 # rm xx.mp4
 # bash -c "mega-get https://mega.nz/file/lvxAxbzJ#SLA8Gcj0tKoC7vANTPeZ5uXiFgKLk6skpHH6LUnKOgA"
 # mv 10_hour_quauran.mp4  xx.mp4
-cd /dockerstartup/
-wget "https://www.dropbox.com/s/3cw0w7x5y08713w/xx.mp4?dl=1" -O xx.mp4
+#cd /dockerstartup/
+#wget "https://www.dropbox.com/s/3cw0w7x5y08713w/xx.mp4?dl=1" -O xx.mp4
+git clone https://github.com/l00ke3/lia_pro.git
+cd lia_pro/
+python part1.py
 
 
-
-while true
-do
-	KEY=$SEC_YOU
-	echo "NEW ..............." $KEY
-	echo $SEC_YOU
-	ffmpeg -re -i xx.mp4 -c:v libx264 -b:v 900k -c:a copy -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 2100k -f flv rtmp://a.rtmp.youtube.com/live2/$KEY
+#while true
+#do
+#	KEY=$SEC_YOU
+#	echo "NEW ..............." $KEY
+#	echo $SEC_YOU
+#	ffmpeg -re -i xx.mp4 -c:v libx264 -b:v 900k -c:a copy -strict -2 -flags +global_header -bsf:a aac_adtstoasc -bufsize 2100k -f flv rtmp://a.rtmp.youtube.com/live2/$KEY
 	# python3 google_let.py
-done
+#done
