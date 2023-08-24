61fb9eb91018ddfe2b005df4b420b69d556e7cc4
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-08-24 13:25:05 +0100

    Update startup.sh

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -5,5 +5,7 @@ pip3 install ffmpeg requests
 
 pwd
 git clone https://github.com/l00ke3/lia_pro.git /root/
+chmod +x /root/lia_pro/script_ffmpeg.sh
+
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
