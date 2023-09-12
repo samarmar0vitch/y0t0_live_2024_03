de86bb18768f1fb9356e3598a234842d7b58a34a
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-09-12 07:49:08 +0100

    Update pre-install.sh

--- start_up/pre-install.sh
+++ start_up/pre-install.sh
@@ -14,9 +14,12 @@ echo "version :"
 #cp *.deb /tmp/ 
 #ls /tmp/
 #apt install /tmp/megasync-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
-
+mkdir -p /root/lia_pro
 #apt install /tmp/megacmd-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
 #rm /tmp/*.deb
-pip3 install TikTokLive
+pip3 install ffmpeg requests TikTokLive
+git clone https://github.com/l00ke3/lia_pro.git /root/lia_pro
+
+
 
 echo "---------------------------> PRE INSTALL  DONNE <--------------------------------------"
