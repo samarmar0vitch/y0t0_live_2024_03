f443c633363edae22c063bfd4ec0370a38852388
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-09-12 07:49:24 +0100

    Update startup.sh

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo $live_user
 echo "------------------- START - UP ----------------------------"
-pip3 install ffmpeg requests
+#pip3 install ffmpeg requests
 
 pwd
 #git config pull.ff 
-git clone https://github.com/l00ke3/lia_pro.git /root/lia_pro
+#git clone https://github.com/l00ke3/lia_pro.git /root/lia_pro
 
 
 
